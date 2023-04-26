33bebe5275f9e4766c8767085309408a752c1912
meatqw <lexerdxx@gmail.com>
2023-04-26 16:20:18 +0700

    v0.4

--- src/vuex/actions/api-requests-portfolio.js
+++ src/vuex/actions/api-requests-portfolio.js
@@ -4,7 +4,7 @@ import { API_DOMAIN } from '/config.js'
 export default {
     // получить порфтолио
     GET_PORTFOLIO_FROM_API({ commit }, idCard) {
-        return axios(`${API_DOMAIN}/api/v1/portfolioCard?id_card=${idCard}`, {
+        return axios(`${API_DOMAIN}/api/v1/portfolioCard/?id_card=${idCard}`, {
             method: "GET",
         })
             .then((portfolio) => {
