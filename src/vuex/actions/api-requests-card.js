adc0910c2226bd6d3269ecff58f9881ee6ec4c3a
olega211 <106370448+olega211@users.noreply.github.com>
2023-05-27 21:44:27 +0700

    fix сслыки в стор

--- src/vuex/actions/api-requests-card.js
+++ src/vuex/actions/api-requests-card.js
@@ -4,7 +4,7 @@ import { API_DOMAIN } from '/config.js'
 export default {
     // получить карточку
     GET_CARD_FROM_API({ commit }, link) {
-        return axios(`${API_DOMAIN}/api/v1/cardLink/?link=${link}&token=bm01wd-8ba97ec799c1950c4c12497a1569a9b3`, {
+        return axios(`${API_DOMAIN}/api/v1/cardLink?link=${link}&token=bm01wd-8ba97ec799c1950c4c12497a1569a9b3`, {
             method: "GET",
         })
             .then((card) => {
