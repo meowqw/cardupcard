64b7c6e18b920385f23eedf8308c9a47079a9374
Mikhail <miksmirnov01@gmail.com>
2023-06-21 22:20:56 +0700

    swiper

--- src/components/card-main.vue
+++ src/components/card-main.vue
@@ -1,9 +1,44 @@
 <template>
-    <router-view> </router-view>
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>
+      <div><card-home /></div>
+    </swiper-slide>
+    <swiper-slide>
+      <div><card-portfolio /></div>
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import cardHome from "./card-home.vue";
+import cardPortfolio from "./card-portfolio.vue";
+
 export default {
-  name: "card-main"
-}
+  name: "card-main",
+  components: {
+    Swiper,
+    SwiperSlide,
+    cardHome,
+    cardPortfolio,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '@/router/router';
-import store from '@/vuex/store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router/router";
+import store from "@/vuex/store";
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).mount("#app");
